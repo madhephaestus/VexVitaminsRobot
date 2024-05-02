ba683d68fa06e5c97669c60683510db0cfc18498
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-02 08:53:18 -0400

    updated wheel location

--- FixedDriveEngine.groovy
+++ FixedDriveEngine.groovy
@@ -22,18 +22,18 @@ return new com.neuronrobotics.sdk.addons.kinematics.IDriveEngine (){
 	}
 	@Override
 	public void DriveArc(MobileBase base, TransformNR newPose, double seconds) {
-		newPose = newPose.inverse()
+		//newPose = newPose.inverse()
 		//println "Move "+newPose.toSimpleString()
 		try {
 			HashMap<DHParameterKinematics,MobileBase> wheels = getDrivable(base)
 			for(DHParameterKinematics LimbWithWheel:wheels.keySet()){
 				MobileBase wheelSource=wheels.get(LimbWithWheel);
-				TransformNR global= base.getFiducialToGlobalTransform();
-				if(global==null){
-					global=new TransformNR()
-					base.setGlobalToFiducialTransform(global)
-				}
-				global=global.times(newPose);// new global pose
+//				TransformNR global= base.getFiducialToGlobalTransform();
+//				if(global==null){
+//					global=new TransformNR()
+//					base.setGlobalToFiducialTransform(global)
+//				}
+//				global=global.times(newPose);// new global pose
 				TransformNR tipCheck = new TransformNR(0,0,1)
 				int wheelIndex = LimbWithWheel.getNumberOfLinks()-1
 				//println "Wheel "+LimbWithWheel.getScriptingName()+" index "+wheelIndex
@@ -50,9 +50,34 @@ return new com.neuronrobotics.sdk.addons.kinematics.IDriveEngine (){
 					-wheelStarting.getY()+zvect.getY(),
 					-wheelStarting.getZ()+zvect.getZ()
 					)
-				double orentAngle =Math.atan2(orentation.getY(),orentation.getX())
-				println "Z pointing in "+Math.toDegrees(orentAngle)
+				double orentAngle =Math.toDegrees( Math.atan2(orentation.getY(),orentation.getX()))
 				
+				TransformNR newWheelPose = newPose.times(wheelStarting)
+				double newX = newWheelPose.getX()-wheelStarting.getX()
+				double newY = newWheelPose.getY()-wheelStarting.getY()
+				double newDelta = Math.sqrt(Math.pow(newX, 2)+Math.pow(newY, 2))
+				double deltaOrentation = Math.toDegrees(Math.atan2(newY,newX))
+				if(deltaOrentation>90||deltaOrentation<-90)
+					newDelta=-newDelta
+				if(orentAngle>0) {
+					//println "Left"
+				}else {
+					//println "Right"
+					newDelta=-newDelta
+				}
+				
+				double theta = 180.0 * newDelta/(LimbWithWheel.getDH_R(wheelIndex)*Math.PI)
+				
+				double[] currVal=LimbWithWheel.getCurrentJointSpaceVector();
+				try{
+					currVal[wheelIndex]+= theta;
+					double best = LimbWithWheel.getBestTime(currVal);
+					if(best>seconds)
+						seconds=best;
+					LimbWithWheel.setDesiredJointAxisValue(wheelIndex,currVal[wheelIndex],seconds);
+				}catch(Exception e){
+						e.printStackTrace(System.out)
+				}
 			}
 		}catch(Throwable t) {
 			t.printStackTrace()
