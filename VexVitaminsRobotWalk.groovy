9a57ee83c2b143c3a43652f4398d603c3636afbe
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-02 08:54:44 -0400

    adding fixed engine

--- VexVitaminsRobotWalk.groovy
+++ VexVitaminsRobotWalk.groovy
@@ -3,19 +3,18 @@ import com.neuronrobotics.sdk.addons.kinematics.IDriveEngine
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 
-// code here
-
 return new com.neuronrobotics.sdk.addons.kinematics.IDriveEngine (){
 	IDriveEngine walking = ScriptingEngine.gitScriptRun("https://github.com/OperationSmallKat/Marcos.git", "MarcosWalk.groovy")
 	IDriveEngine driving = ScriptingEngine.gitScriptRun("https://github.com/NeuronRobotics/NASACurisoity.git", "DriveEngine.groovy")
+	IDriveEngine fixed = ScriptingEngine.gitScriptRun("https://github.com/madhephaestus/VexVitaminsRobot.git", "FixedDriveEngine.groovy")
 	
 	@Override
 	public void DriveArc(MobileBase base, TransformNR newPose, double seconds) {
-		IDriveEngine engine=driving;
+		IDriveEngine engine=fixed;
 		if(base.getLegs().size()>0) {
 			engine=walking;
 		}else if(base.getDrivable().size()>0) {
-			engine=driving;
+			engine=fixed;
 		}else if(base.getSteerable().size()>0) {
 			engine=driving;
 		}
