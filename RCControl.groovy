3cc3893e63edc1550e1db6c6a9a4e36f31e60e3f
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-21 12:51:29 -0400

    adding IMU updates

--- RCControl.groovy
+++ RCControl.groovy
@@ -6,6 +6,8 @@ import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
 import com.neuronrobotics.bowlerstudio.threed.*;
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.addons.kinematics.imu.IMUUpdate
+import com.neuronrobotics.sdk.addons.kinematics.imu.IMUUpdateListener
 import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 import com.neuronrobotics.sdk.common.DeviceManager
@@ -15,23 +17,50 @@ MobileBase base=DeviceManager.getSpecificDevice( "VexVitaminsRobot",{ScriptingEn
 		"VexVitaminsRobot.xml", 
 		null )})
 
-println "Now we will move just one arm"
-DHParameterKinematics arm = base.getAppendages().get(0)
+TransformNR measuredPose=null ;
+IMUUpdateListener listener =new IMUUpdateListener() {
 
-double zLift=25
-println "Start from where the arm already is and move it from there with absolute location"
-TransformNR current = arm.getCurrentPoseTarget();
-
-TransformNR absolute = new TransformNR(400,0,5)
-
-current.translateZ(zLift);
-arm.setDesiredTaskSpaceTransform(absolute,  2.0);
-ThreadUtil.wait(2000)// wait for the arm to fully arrive
-
-// move the ring in positive Y by 50 mm
-TransformNR move = new TransformNR(-5,0,0,new RotationNR())
-double toSeconds=0.03//30 ms for each increment
-for(int i=0;i<40;i++){
-	base.DriveArc(move, toSeconds);
-	ThreadUtil.wait((int)toSeconds*1000)
+	@Override
+	public void onIMUUpdate(IMUUpdate update) {
+		measuredPose=new TransformNR(0,0,0,new RotationNR(	-update.getxAcceleration(),
+									update.getyAcceleration()-90,	update.getzAcceleration()	))
+									.times(new TransformNR(0,0,0,new RotationNR(180,0,0)))
+									.times(new TransformNR(0,0,0,new RotationNR(0,90,0)))
+	}
+	
 }
+// Add an IMU listener
+base.getImu().addvirtualListeners(listener)
+
+try {
+	println "Now we will move just one arm"
+	DHParameterKinematics arm = base.getAppendages().get(0)
+	
+	double zLift=25
+	println "Start from where the arm already is and move it from there with absolute location"
+	TransformNR current = arm.getCurrentPoseTarget();
+	
+	TransformNR absolute = new TransformNR(400,0,5)
+	
+	current.translateZ(zLift);
+	arm.setDesiredTaskSpaceTransform(absolute,  2.0);
+	ThreadUtil.wait(2000)// wait for the arm to fully arrive
+	
+	// move the ring in positive Y by 50 mm
+	TransformNR move = new TransformNR(-5,0,0,new RotationNR(0,5,0))
+	double toSeconds=0.03//30 ms for each increment
+	for(int i=0;i<40;i++){
+		base.DriveArc(move, toSeconds);
+		ThreadUtil.wait((int)toSeconds*1000)
+		if(measuredPose!=null)
+			println "Heading is "+measuredPose.toSimpleString()
+	}
+}catch(Throwable t) {
+	t.printStackTrace(System.out)
+}
+// remove IMU listener at end of script
+base.getImu().removevirtualListeners(listener)
+
+
+
+
